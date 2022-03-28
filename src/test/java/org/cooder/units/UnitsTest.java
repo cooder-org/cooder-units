03ef43ee4c604d649a8e3d7d20b96fefe765b417
yanxiyue <wuling@cooder.org>
2022-03-28 16:32:27 +0800

    add addAlias for Units

--- src/test/java/org/cooder/units/UnitsTest.java
+++ src/test/java/org/cooder/units/UnitsTest.java
@@ -1,6 +1,7 @@
 package org.cooder.units;
 
 import javax.measure.Unit;
+import javax.measure.quantity.Area;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -19,6 +20,17 @@ public class UnitsTest {
 
         u = Units.symbolFor("K");
         Assert.assertTrue(u.equals(tech.units.indriya.unit.Units.KELVIN));
+    }
+
+    @Test
+    public void testAddAlias() {
+        Units.addAlias(Units.平方米, "㎡");
+        Unit<?> u = Units.nameFor("㎡");
+        Assert.assertTrue(u.equals(Units.平方米));
+
+        UnitNumber<Area> num = UnitNumber.parse("5 ㎡").asType(Area.class);
+        Assert.assertTrue(num.getUnit().isEquivalentTo(Units.平方米));
+        Assert.assertTrue("5 m²".equals(num.toString()));
     }
 
     @Test
